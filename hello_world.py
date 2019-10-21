bb4855d56b7f8817faec1e3da5963232f7e01da3
katebartnik <oferta.bartnik@gmail.com>
2019-10-21 19:50:46 +0200

    Zmiany w skryptach

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
 #
-#
+# Testowy skrypt na potrzeby Github
 #
 print("Ruda tańczy jak szalona")
+a = 123.45
+print(a)
---
+++ napisy.py
@@ -0,0 +1,14 @@
+#
+# Typ danych: napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'Tu jest "cudzysłów"'
+napis4 = "A to jest \"cudzysłów\" "
+napis5 = "Znak specjalny \t \n \r"
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis1 to ", dlugosc, "znaków")
+
+
+
