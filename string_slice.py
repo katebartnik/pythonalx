52f66512a996901e569fb9ff49326255eb10691b
katebartnik <oferta.bartnik@gmail.com>
2019-10-21 20:33:22 +0200

    Zmiany w skryptach

---
+++ string_slice.py
@@ -0,0 +1,21 @@
+#
+#array slicing
+#
+
+s = "Hello"
+print(s[0:5]) #od indeksu 0 do 4 włącznie
+print(s[-3]) #3ci znak od końca
+
+s = "Ruda tańczy jak szalona"
+arr = s.split(" ")
+print(arr[0]) # pierwszy element listy
+print(s[0:16:2]) #co drugi znak od indeksu 0 do 15 włącznie
+print(s[::3]) #cały string co 3 znak
+
+#reverse w Pythonie
+print(s[::-1]) #który język zrobi to krócej :P
+
+
+
+
+
