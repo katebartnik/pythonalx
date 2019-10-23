4e59b5188b526c9379d343a8f9c4272078dce49f
katebartnik <oferta.bartnik@gmail.com>
2019-10-23 18:21:47 +0200

    Dokonczenie

---
+++ arytmetyka.py
@@ -0,0 +1,16 @@
+#1. Przypisz do zmiennych x i y jakieś wartośli liczbowe
+#2. Korzystając z funkcji print wypisz na ekranie podstawowe działania arytmetyczne (dodawanie, odejmowanie, dzielenie, mnożenie, dzielenie całkowite, reszta z dzielenia, potęgowanie)
+#3. uruchom program kilka razy
+
+x = -2
+y = 5
+print("x", x, "y", y)
+print("dodawanie", x+y)
+print("odejmowanie", x-y)
+print("dzielenie", x/y)
+print("mnożenie", x*y)
+print("dzielenie calkowite", x//y)
+print("reszta z dzielenia", x%y)
+print("potęgowanie", x**y)
+
+
---
+++ napisy.py
@@ -0,0 +1,34 @@
+#
+# Typ danych: napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'Tu jest "cudzysłów"'
+napis4 = "A to jest \"cudzysłów\" "
+napis5 = "Znak specjalny \t \n \r"
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis1 to ", dlugosc, "znaków")
+
+#wiek = input("Podaj swój wiek")
+#print("Twój wiek to:", wiek.strip())
+
+s = "Ruda tańczy jak szalona"
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.center(100)) #centrowanie
+print(s.replace("R","D")) #zmiana znaków
+print("Czy liczba: ",s.isdigit()) #sprawdzanie czy string jest liczba
+
+print("4-ta litera:", s[3]) #pobierz 4 literę napisu
+print("Litera przedostatnia:", s[-2])
+
+
+
+
+
+
+
+
