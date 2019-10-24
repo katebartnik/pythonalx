6e6adfc5521166ace56bb3a9dbd9f89ea2454290
Kasia <oferta.bartnik@gmail.com>
2019-10-24 20:25:35 +0200

    uzupełnienie zadania

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,17 +1,23 @@
 
-x, y = 200, 0
+x, y = 50, 50
 
 if x > 100 or x < 0 or y > 100 or y < 0:
     print("Jesteś poza planszą")
 elif x > 90 and y > 90:
     print("Jesteś w GPR")
 elif x > 90 and y < 10:
-    print("Jestes w DPR")
+    print("Jesteś w DPR")
 elif x < 10 and y > 90:
-    print("Jestes w LGR")
+    print("Jesteś w LGR")
 elif x < 10 and y < 10:
     print("Jesteś w LDR")
 elif x > 90:
-    print("Jesteś na PK")
+    print("Jeteś na PK")
 elif x < 10:
-    print("jgbhjh")
+    print("Jeteś na LK")
+elif y > 90:
+    print("Jeteś na GK")
+elif y < 10:
+    print("Jeteś na DK")
+else:
+    print("Jesteś w Centrum")
