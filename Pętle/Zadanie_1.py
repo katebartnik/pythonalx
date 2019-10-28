53f503f5ba89864a31a56ec067b43f6a9240aed6
katebartnik <oferta.bartnik@gmail.com>
2019-10-28 19:37:48 +0100

    petle for

---
+++ Pętle/Zadanie_1.py
@@ -0,0 +1,12 @@
+#Zapytaj użytkowanika o 10 liczba i wypisz ich średnią
+# Skorzystaj z petki for i range,
+
+lista = list(range(10))
+suma = 0
+for x in lista:
+    x = int(input("Podaj wartość: "))
+    suma += 1
+print(suma)
+
+
+
