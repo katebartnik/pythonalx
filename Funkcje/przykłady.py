37d32c68d0c9724e3a95241a07af2ad4cc85fc82
katebartnik <oferta.bartnik@gmail.com>
2019-10-30 20:29:34 +0100

    Funkcje

---
+++ Funkcje/przykłady.py
@@ -0,0 +1,15 @@
+def nazwa_funkcji(): # brak argumentów
+
+    #body
+    #blok
+    print("hello")
+nazwa_funkcji()
+
+def powieksz(x):
+    return x.upper()
+x = powieksz("małe litery")
+print(powieksz("ala"))
+
+print(x)
+
+
