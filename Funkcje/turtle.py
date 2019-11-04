887077f6abef18eb26430c12d37b29f1f1ca6acd
katebartnik <oferta.bartnik@gmail.com>
2019-11-04 19:55:14 +0100

    turtle

--- Funkcje/turtle.py
+++
@@ -1,8 +0,0 @@
-import turtle
-print(help(turtle))
-
-
-turtle.right(90) #skręca w prawo o zadany kat
-turtle.forward(50) #idzie do przodu
-
-turtle.done()
---
+++ Funkcje/turtle_examples.py
@@ -0,0 +1,9 @@
+import turtle
+print(help(turtle))
+
+for i in range(3):
+    turtle.right(120) #skręca w prawo o zadany kat
+    turtle.forward(50) #idzie do przodu
+
+
+turtle.done()
